fc9f38e1e7983a65d93656ddaa6fde32ab48d50c
jtmaston <jtmstn1@gmail.com>
2022-01-14 14:47:59 +0200

    Moved motors to other threads Finalized viewfinder / framebuffer Added proper thread exit Minor performance improvements Implemented training code

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ Config.set('graphics', 'width', f'{1080}')
 Config.set('graphics', 'height', f'{720}')
 Config.set('graphics', 'maxfps', '60')
 
-
-
-
 from kivymd.app import MDApp
 
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 
-
 import kivy
-
 
 from robot import Robot
 from kivy.clock import Clock
@@ -26,56 +21,69 @@ from kivy import Config
 from time import sleep
 
 from threading import Thread
+import asynckivy as ak
 
 class DashboardApp(MDApp):
     def __init__(self):
-        self.running = True
         super(DashboardApp, self).__init__()
         self.robot = Robot()
-        Clock.schedule_interval(self.display_axis_data, 0)
-        #Clock.schedule_interval(self.viewfinder, 0.033)
-        self.motor_callback = Clock.schedule_interval(self.update_motors, 0)
-        self.viewfinder_thread = Thread(target = self.viewfinder, args=(self,))
-        #viewfinder_thread.start()
+
+        Clock.schedule_interval(self.display_axis_data, 0.1)
+        Clock.schedule_interval(self.viewfinder, 0.066)
+        Clock.schedule_interval(self.update_motors, 0.1)
+        #Clock.schedule_interval(self.upd, 0.066)
+
+        self.camera = cv2.VideoCapture(0)
+        self.robot.arm.Arm_serial_set_torque(1)
+        self.camTex = Texture.create(size=(640, 480), colorfmt='rgba')
 
     def build(self, *args, **kwargs):
         super(DashboardApp, self).build(*args, **kwargs)
-        ##self.viewfinder_thread.start()
+        Thread(target=self.robot.poll_axes).start()
 
     def viewfinder(self, *args, **kwargs):
-        self = args[0]
-        #camera = cv2.VideoCapture(0)
+        ret, frame = self.camera.read()
+        cv2.imshow("CV2 Image", frame)
+        buf1 = cv2.flip(frame, 0)
+        buf = buf1.tobytes()
+        #texture1 = 
+        self.camTex.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        self.root.ids['camera'].texture = self.camTex
+        #Thread(target=self.blit, args=(self.camTex, buf, )).start()
 
-        """while self.running:
-            print(self.running)
-            ret, frame = camera.read()
-            cv2.imshow("CV2 Image", frame)
-            buf1 = cv2.flip(frame, 0)
-            buf = buf1.tostring()
-            texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgba')
-            texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            self.root.ids['camera'].texture = texture1
 
-            print(self.root.ids['camera'].texture)
+    def upd( self, pp ):
+        #print(pp)
+        self.root.ids['camera'].texture = self.camTex
 
-            sleep(0.1)"""
-        self.root.ids['camera'].texture = self.robot.framebuffer()
+    @staticmethod
+    def blit(tgt_tex, buf):
+        #print(tgt_tex)
+        tgt_tex.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
 
     def display_axis_data(self, *args, **kwargs):
-        print(self.root.ids['camera'].texture)
-        angles = self.robot.poll_axes()
+        angles = self.robot.axes
         for i in range(0, 5):
             self.root.ids[f'ax{i + 1}_readout'].text = f"Axa {i + 1}: {angles[i]}°"
 
     def update_motors(self, *args, **kwargs):
         self.robot.move([int(self.root.ids[f"ax{i + 1}"].value + 90) for i in range(0,6)])
+        #Thread(target=self.robot.move, args=([int(self.root.ids[f"ax{i + 1}"].value + 90) for i in range(0,6)], )).start()
 
-    def learn(self, *args, **kwargs):
-        #self.motor_callback.cancel()
-        self.robot.reset()
+    def mde_lrn(self):
+        Thread(target=self.robot.toggle_learn).start()
 
-    def on_stop(self, *args, **kwargs):
-        self.runing = False
+    def add(self):
+        self.robot.add_step()
+
+    def rm(self):
+        self.robot.remove_step()
+
+    def strt(self):
+        Thread(target=self.robot.execute).start()
+
+    def on_stop(self):
+        self.robot.alive = False
 
 if __name__ == '__main__':
     DashboardApp().run()
--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ from time import sleep
 import cv2
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
+import asynckivy as ak
 
 class Robot:
     def __init__(self):
@@ -11,62 +12,60 @@ class Robot:
         self.camera = cv2.VideoCapture(0)
         self.steps = []
         self.camera = None
+        self.disable_move = False
+        self.done = True
+        self.axes = None
+        self.alive = True
+
+    def halt(self):
+        self.arm.Arm_serial_set_torque( 10 )
+        self.arm.done = True
 
     def framebuffer(self):
         ret, frame = self.camera.read()
         cv2.imshow("CV2 Image", frame)
         buf1 = cv2.flip(frame, 0)
         buf = buf1.tostring()
-        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgba')
+        texture1 = Texture.create(size=(640, 480), colorfmt='rgba')
         texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         return texture1
 
     def poll_axes(self):
-        angles = [self.arm.Arm_serial_servo_read(i + 1) for i in range(0, 5)]
-        return [i if i is not None else 0 for i in angles]
+        while self.alive:
+            angles = [self.arm.Arm_serial_servo_read(i + 1) for i in range(0, 6)]
+            self.axes = [i if i is not None else 0 for i in angles]
+            sleep(0.1)
 
     def move(self, target_angles):
-        if target_angles[-1] < 25:
-            target_angles[-1] = 25
-        #print(target_angles)
-        self.arm.Arm_serial_servo_write6_array(target_angles, 500)
+        if not self.disable_move:
+            if target_angles[-1] < 25:
+                target_angles[-1] = 25
+            self.arm.Arm_serial_servo_write6_array(target_angles, 500)
 
-    def reset(self, callback = None):
+    def reset(self):
         self.arm.Arm_serial_set_torque(1)
-        self.arm.Arm_serial_servo_write6(90, 90, 90, 0, 90, 90, 500)      # reset position
-        #sleep(1)
-        if callback is not None:
-            callback.cancel()
+        self.arm.Arm_serial_servo_write6_array([90, 90, 90, 90, 90, 90], 500)
+        #Clock.usleep( 2000000 )
+        sleep(2)
 
-    def mode_train(self):
+    def toggle_learn(self):
+        self.disable_move = True
         self.reset()
+        print("here")
+        self.arm.Arm_serial_set_torque( 10 ) # disable torque
 
-        global x
-        done = False
-        global targets
-        self.arm.Arm_serial_set_torque(10)
-        print("Please start moving the robot")
-        print("Enter saves a step, q stops")
+    def add_step(self):
+        self.steps.append(self.poll_axes())
 
-        while not done:
-            print(f"Step {x}: ", end="")
-            x += 1
-            c = input()
-            if c == "q":
-                print("Robot will move to default position! Stand clear!")
-                done = True
-            else:
-                targets.append(getPoint(bot))
-        sleep(3)
+    def remove_step(self):
+        self.steps.pop()
 
-        print(targets)
+    def execute(self):
+        self.done = False
+        self.arm.Arm_serial_set_torque(1)
+        while self.alive:
+            for i in self.steps:
+                if i is not None:
+                    self.arm.Arm_serial_servo_write6_array(i, 2000)
+                    sleep(2)
 
-        while True:
-            self.reset()
-            print("Waiting a little bit")
-            sleep(3)
-            for i in targets:
-                self.arm.Arm_serial_servo_write6_array(i, 1000)
-                sleep(2)
-                print("Returning to position")
-                sleep(3)

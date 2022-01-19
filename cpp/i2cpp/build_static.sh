9026dedf5ce8dd75c0f82cb5f22a63fe4a4aad6e
jtmaston <jtmstn1@gmail.com>
2022-01-19 12:22:56 +0200

    Reorganized whole project Cleanup of files, moved python code to a legacy folder ( to be deprecated ) Added CMakeLists file

--- cpp/i2cpp/build_static.sh
+++
@@ -1,3 +0,0 @@
-g++ -c Arm_lib.cpp
-ar rvs libarm.a Arm_lib.o
-g++ main.cpp libarm.a -li2c
--- cpp/i2cpp/tester.py
+++
@@ -1,5 +0,0 @@
-from Arm_Lib import Arm_Device
-
-dev = Arm_Device()
-
-dev.Arm_serial_servo_write(0, 45, 3000)
